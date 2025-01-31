e18525cf4d457b0e5a13106542aea5edf8aeef6e
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-01-31 20:27:17 +0400

    Add test token contract

---
+++ contracts/test/TokenTest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.28;
+
+import {ERC20Permit, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract TokenTest is ERC20Permit {
+    string private constant NAME = "TokenTest";
+    string private constant SYMBOL = "TT";
+
+    constructor()
+        ERC20(NAME, SYMBOL)
+        ERC20Permit(NAME)
+    {}
+
+    function mint(address account_, uint256 amount_) external {
+        _mint(account_, amount_);
+    }
+}
