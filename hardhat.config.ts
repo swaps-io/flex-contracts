33e645aad1965c5e7dbbb484e03044829e26452c
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-04 17:06:18 +0400

    Configure coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import type { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox-viem';
 import 'hardhat-contract-sizer';
 import 'hardhat-gas-reporter';
+import 'solidity-coverage';
 
 const config: HardhatUserConfig = {
   solidity: {
