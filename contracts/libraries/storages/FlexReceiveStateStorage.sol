3cab98e971252c2dff6e819c735d8655c7801fe6
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-06 19:38:36 +0400

    Fix state storage slot comments

--- contracts/libraries/storages/FlexReceiveStateStorage.sol
+++ contracts/libraries/storages/FlexReceiveStateStorage.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.28;
 library FlexReceiveStateStorage {
     function data() internal pure returns (mapping(bytes32 bucket => bytes32 state) storage d) {
         assembly {
-            // keccak256(abi.encode(uint256(keccak256("com.swaps-io.flex.FlexReceiveStateStorage")) - 1)) & ~bytes32(uint256(0xff))
+            // keccak256("com.swaps-io.flex.FlexReceiveStateStorage") & ~bytes32(uint256(0xff))
             d.slot := 0xbdc9d446a12e6027b20b648f60cb0719a85ec0689478a6e0ff8c8ee6155e9f00
         }
     }
--- contracts/libraries/storages/FlexSendStateStorage.sol
+++ contracts/libraries/storages/FlexSendStateStorage.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.28;
 library FlexSendStateStorage {
     function data() internal pure returns (mapping(bytes32 bucket => bytes32 state) storage d) {
         assembly {
-            // keccak256(abi.encode(uint256(keccak256("com.swaps-io.flex.FlexSendStateStorage")) - 1)) & ~bytes32(uint256(0xff))
+            // keccak256("com.swaps-io.flex.FlexSendStateStorage") & ~bytes32(uint256(0xff))
             d.slot := 0xa9db9766eb1152a1d5954ecac4572d31a2211af00547d4afbabfb39b0b3bdf00
         }
     }
