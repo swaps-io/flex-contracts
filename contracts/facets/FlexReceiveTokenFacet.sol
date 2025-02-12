f8c618fb85685b3e760e3049181485b92000552a
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-12 11:54:15 +0400

    Update receive token facet w/ struct changes

--- contracts/facets/FlexReceiveTokenFacet.sol
+++ contracts/facets/FlexReceiveTokenFacet.sol
@@ -22,25 +22,24 @@ contract FlexReceiveTokenFacet is IFlexReceiveToken {
     bytes32 private immutable _domain = FlexDomain.calc(IFlexReceiveToken.flexReceiveToken.selector);
 
     function flexReceiveToken(
-        bytes32 receiveData0_, // Content: deadline (48), nonce (40), receiver flags (8), receiver (160)
+        bytes32 receiveData0_, // Content: signer flags (2), deadline (46), nonce (48), receiver (160)
         bytes32 receiveData1_, // Content: token amount (256)
         bytes32 receiveData2_, // Content: <unused> (96), token (160)
         bytes32[] calldata componentBranch_,
         bytes calldata receiverSignature_
     ) external override {
-        FlexDeadlineConstraint.validate(uint256(receiveData0_) >> 208);
+        FlexDeadlineConstraint.validate(uint256(receiveData0_ << 2) >> 210);
 
-        bytes32 componentHash = FlexEfficientHash.calc(_domain, receiveData0_, receiveData1_, receiveData2_);
-        bytes32 orderHash = MerkleProof.processProofCalldata(componentBranch_, componentHash);
+        bytes32 orderHash = FlexEfficientHash.calc(receiveData0_, receiveData1_, receiveData2_);
+        orderHash = FlexEfficientHash.calc(_domain | bytes32(uint256(uint160(msg.sender))), orderHash);
+        orderHash = MerkleProof.processProofCalldata(componentBranch_, orderHash);
 
         address receiver = address(uint160(uint256(receiveData0_)));
-        FlexSignatureConstraint.validate(uint256(receiveData0_ >> 160), receiver, orderHash, receiverSignature_);
+        FlexSignatureConstraint.validate(uint256(receiveData0_ >> 254), receiver, orderHash, receiverSignature_);
 
-        uint96 nonce = uint40(uint256(receiveData0_) >> 168);
-        FlexReceiveStateUpdate.toReceived(receiver, nonce, orderHash);
+        FlexReceiveStateUpdate.toReceived(receiver, uint48(uint256(receiveData0_) >> 160), orderHash);
 
-        address token = address(uint160(uint256(receiveData2_)));
-        SafeERC20.safeTransferFrom(IERC20(token), msg.sender, address(this), uint256(receiveData1_));
+        SafeERC20.safeTransferFrom(IERC20(address(uint160(uint256(receiveData2_)))), msg.sender, address(this), uint256(receiveData1_));
 
         emit FlexReceive(orderHash);
     }
