ed37e92f7e559f8203a62a3fad6916a575b8d1d0
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-12 12:28:48 +0400

    Update refund native facet w/ struct changes

--- contracts/facets/FlexRefundNativeFacet.sol
+++ contracts/facets/FlexRefundNativeFacet.sol
@@ -20,9 +20,8 @@ contract FlexRefundNativeFacet is IFlexRefundNative {
     bytes32 private immutable _domain = FlexDomain.calc(IFlexRefundNative.flexRefundNative.selector);
 
     function flexRefundNative(
-        bytes32 receiveData0_, // Content: deadline (48), nonce (40), receiver flags (8), receiver (160)
+        bytes32 receiveData0_, // Content: signer flags (2), deadline (46), nonce (48), receiver (160)
         bytes32 receiveData1_, // Content: amount (256)
-        bytes32 receiveData2_, // Content: <unused> (96), sender (160)
         bytes32 refundData0_, // Content: key hash (256)
         bytes32 refundData1_, // Content: <unused> (96), refund receiver (160)
         bytes32 refundKey_,
@@ -32,16 +31,13 @@ contract FlexRefundNativeFacet is IFlexRefundNative {
     ) external override {
         FlexKeyConstraint.validate(refundData0_, refundKey_);
 
-        bytes32 componentHash = FlexEfficientHash.calc(receiveData0_, receiveData1_, receiveData2_);
-        componentHash = FlexEfficientHash.calc(_domain, refundData0_, refundData1_, componentHash);
-        bytes32 orderHash = MerkleProof.processProofCalldata(componentBranch_, componentHash);
+        bytes32 orderHash = FlexEfficientHash.calc(receiveData0_, receiveData1_);
+        orderHash = FlexEfficientHash.calc(_domain | bytes32(uint256(uint192(uint256(refundData0_)))), refundData1_, orderHash);
+        orderHash = MerkleProof.processProofCalldata(componentBranch_, orderHash);
 
-        address receiver = address(uint160(uint256(receiveData0_)));
-        uint96 nonce = uint40(uint256(receiveData0_) >> 168);
-        FlexReceiveStateUpdate.toRefunded(receiver, nonce, orderHash, receiveHashBefore_, receiveOrderHashesAfter_);
+        FlexReceiveStateUpdate.toRefunded(address(uint160(uint256(receiveData0_))), uint48(uint256(receiveData0_) >> 160), orderHash, receiveHashBefore_, receiveOrderHashesAfter_);
 
-        receiver = address(uint160(uint256(refundData1_)));
-        Address.sendValue(payable(receiver), uint256(receiveData1_));
+        Address.sendValue(payable(address(uint160(uint256(refundData1_)))), uint256(receiveData1_));
 
         emit FlexRefund(orderHash);
     }
