9f8248cd7d52cfd279ae4db20a54975473071184
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-12 12:24:56 +0400

    Update confirm native facet w/ struct changes

--- contracts/facets/FlexConfirmNativeFacet.sol
+++ contracts/facets/FlexConfirmNativeFacet.sol
@@ -20,9 +20,8 @@ contract FlexConfirmNativeFacet is IFlexConfirmNative {
     bytes32 private immutable _domain = FlexDomain.calc(IFlexConfirmNative.flexConfirmNative.selector);
 
     function flexConfirmNative(
-        bytes32 receiveData0_, // Content: deadline (48), nonce (40), receiver flags (8), receiver (160)
+        bytes32 receiveData0_, // Content: signer flags (2), deadline (46), nonce (48), receiver (160)
         bytes32 receiveData1_, // Content: amount (256)
-        bytes32 receiveData2_, // Content: <unused> (96), sender (160)
         bytes32 confirmData0_, // Content: key hash (256)
         bytes32 confirmKey_,
         bytes32[] calldata componentBranch_,
@@ -31,13 +30,12 @@ contract FlexConfirmNativeFacet is IFlexConfirmNative {
     ) external override {
         FlexKeyConstraint.validate(confirmData0_, confirmKey_);
 
-        bytes32 componentHash = FlexEfficientHash.calc(receiveData0_, receiveData1_, receiveData2_);
-        componentHash = FlexEfficientHash.calc(_domain, confirmData0_, componentHash);
-        bytes32 orderHash = MerkleProof.processProofCalldata(componentBranch_, componentHash);
+        bytes32 orderHash = FlexEfficientHash.calc(receiveData0_, receiveData1_);
+        orderHash = FlexEfficientHash.calc(_domain | bytes32(uint256(uint192(uint256(confirmData0_)))), orderHash);
+        orderHash = MerkleProof.processProofCalldata(componentBranch_, orderHash);
 
         address receiver = address(uint160(uint256(receiveData0_)));
-        uint96 nonce = uint40(uint256(receiveData0_) >> 168);
-        FlexReceiveStateUpdate.toConfirmed(receiver, nonce, orderHash, receiveHashBefore_, receiveOrderHashesAfter_);
+        FlexReceiveStateUpdate.toConfirmed(receiver, uint48(uint256(receiveData0_) >> 160), orderHash, receiveHashBefore_, receiveOrderHashesAfter_);
 
         Address.sendValue(payable(receiver), uint256(receiveData1_));
 
--- contracts/interfaces/IFlexConfirmNative.sol
+++ contracts/interfaces/IFlexConfirmNative.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.28;
 
 interface IFlexConfirmNative {
     function flexConfirmNative(
-        bytes32 receiveData0, // Content: deadline (48), nonce (40), receiver flags (8), receiver (160)
+        bytes32 receiveData0, // Content: signer flags (2), deadline (46), nonce (48), receiver (160)
         bytes32 receiveData1, // Content: amount (256)
-        bytes32 receiveData2, // Content: <unused> (96), sender (160)
         bytes32 confirmData0, // Content: key hash (256)
         bytes32 confirmKey,
         bytes32[] calldata componentBranch,
