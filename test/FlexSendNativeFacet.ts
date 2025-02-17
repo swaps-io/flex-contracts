db11d5dd8a1a22ccef71dfbea1100650c9c94a21
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-17 19:33:18 +0400

    Update send native tests

--- test/FlexSendNativeFacet.ts
+++ test/FlexSendNativeFacet.ts
@@ -1,30 +1,18 @@
 import { viem } from 'hardhat';
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox-viem/network-helpers';
-import { ContractTypesMap } from 'hardhat/types/artifacts';
 import { expect } from 'chai';
-import {
-  Address,
-  bytesToHex,
-  getAbiItem,
-  Hex,
-  toFunctionSelector,
-  toFunctionSignature,
-  zeroAddress,
-} from 'viem';
+import { bytesToHex, Hex, parseEventLogs, zeroAddress } from 'viem';
 
 import {
-  flexCalcSendNativeBranch,
   flexCalcSendNativeHash,
   flexCalcTree,
   flexCalcTreeHash,
-  flexEncodeSendNativeData0,
-  flexEncodeSendNativeData1,
-  flexEncodeSendNativeData2,
   flexCalcAccumulatorHash,
+  flexEncodeSendNativeData,
+  flexCalcBranch,
 } from '../@swaps-io/flex-sdk';
 
 const IMAGINARY_COMPONENTS = 3; // Implied in order, but not used here
-const INSIDE_DIAMOND = false; // Diamond or standalone
 
 describe('FlexSendNativeFacet', function () {
   async function deployFixture() {
@@ -32,281 +20,43 @@ describe('FlexSendNativeFacet', function () {
 
     const [walletClient, resolverClient] = await viem.getWalletClients();
 
-    const sendNativeDomain = '0xabababababababababababababababababababababababababababababababab';
-    const sendTokenDomain = '0xcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd'; // For standalone
-
-    let flex: { address: Address };
-    let flexSendNativeFacet: ContractTypesMap['FlexSendNativeFacet'];
-    let flexSendNativeDomainFacet: ContractTypesMap['FlexSendNativeDomainFacet'];
-    let flexSendTimeFacet: ContractTypesMap['FlexSendTimeFacet'];
-    let flexSendHashFacet: ContractTypesMap['FlexSendHashFacet'];
-
-    if (INSIDE_DIAMOND) {
-      const diamondCutFacet = await viem.deployContract('DiamondCutFacet');
-
-      flexSendNativeFacet = await viem.deployContract('FlexSendNativeFacet', [sendNativeDomain]);
-      flexSendNativeDomainFacet = await viem.deployContract('FlexSendNativeDomainFacet', [sendNativeDomain]);
-      flexSendTimeFacet = await viem.deployContract('FlexSendTimeFacet');
-      flexSendHashFacet = await viem.deployContract('FlexSendHashFacet');
-
-      flex = await viem.deployContract('Diamond', [walletClient.account.address, diamondCutFacet.address]);
-      await walletClient.writeContract({
-        abi: diamondCutFacet.abi,
-        address: flex.address,
-        functionName: 'diamondCut',
-        args: [
-          [
-            {
-              action: 0, // Add
-              facetAddress: flexSendNativeFacet.address,
-              functionSelectors: [
-                toFunctionSelector(
-                  getAbiItem({
-                    abi: flexSendNativeFacet.abi,
-                    name: 'flexSendNative',
-                  }),
-                ),
-              ],
-            },
-            {
-              action: 0, // Add
-              facetAddress: flexSendNativeDomainFacet.address,
-              functionSelectors: [
-                toFunctionSelector(
-                  getAbiItem({
-                    abi: flexSendNativeDomainFacet.abi,
-                    name: 'flexSendNativeDomain',
-                  }),
-                ),
-              ],
-            },
-            {
-              action: 0, // Add
-              facetAddress: flexSendTimeFacet.address,
-              functionSelectors: [
-                toFunctionSelector(
-                  getAbiItem({
-                    abi: flexSendTimeFacet.abi,
-                    name: 'flexSendTime',
-                  }),
-                ),
-              ],
-            },
-            {
-              action: 0, // Add
-              facetAddress: flexSendHashFacet.address,
-              functionSelectors: [
-                toFunctionSelector(
-                  getAbiItem({
-                    abi: flexSendHashFacet.abi,
-                    name: 'flexSendHash',
-                  }),
-                ),
-              ],
-            },
-          ],
-          zeroAddress,
-          '0x',
-        ],
-      });
-    } else {
-      flex = await viem.deployContract(
-        'FlexSendStandalone',
-        [
-          sendNativeDomain,
-          sendTokenDomain,
-        ],
-      );
-
-      flexSendNativeFacet = flex as ContractTypesMap['FlexSendNativeFacet'];
-      flexSendNativeDomainFacet = flex as ContractTypesMap['FlexSendNativeDomainFacet'];
-      flexSendTimeFacet = flex as ContractTypesMap['FlexSendTimeFacet'];
-      flexSendHashFacet = flex as ContractTypesMap['FlexSendHashFacet'];
-    }
+    const flex = await viem.deployContract('FlexStandalone', [zeroAddress]);
 
     return {
       publicClient,
       walletClient,
       resolverClient,
       flex,
-      flexSendNativeFacet,
-      flexSendNativeDomainFacet,
-      flexSendTimeFacet,
-      flexSendHashFacet,
     };
   }
 
-  //
-  if (INSIDE_DIAMOND) {
-  //
-
-    it('Should show FlexSendNativeFacet code', async function () {
-      const { publicClient, flexSendNativeFacet } = await loadFixture(deployFixture);
-
-      const code = await publicClient.getCode({ address: flexSendNativeFacet.address });
-      console.log(`FlexSendNativeFacet code: ${code}`);
-    });
-
-    it('Should show FlexSendNativeDomainFacet code', async function () {
-      const { publicClient, flexSendNativeDomainFacet } = await loadFixture(deployFixture);
-  
-      const code = await publicClient.getCode({ address: flexSendNativeDomainFacet.address });
-      console.log(`FlexSendNativeDomainFacet code: ${code}`);
-    });
-
-    it('Should show FlexSendTimeFacet code', async function () {
-      const { publicClient, flexSendTimeFacet } = await loadFixture(deployFixture);
-  
-      const code = await publicClient.getCode({ address: flexSendTimeFacet.address });
-      console.log(`FlexSendTimeFacet code: ${code}`);
-    });
-
-    it('Should show FlexSendHashFacet code', async function () {
-      const { publicClient, flexSendHashFacet } = await loadFixture(deployFixture);
-
-      const code = await publicClient.getCode({ address: flexSendHashFacet.address });
-      console.log(`FlexSendHashFacet code: ${code}`);
-    });
-
-    it('Should show FlexSendNativeFacet function selectors', async function () {
-      const { flexSendNativeFacet } = await loadFixture(deployFixture);
-
-      console.log('FlexSendNativeFacet selectors:');
-      for (const abi of flexSendNativeFacet.abi) {
-        if (abi.type !== 'function') {
-          continue;
-        }
-  
-        const item = getAbiItem({ abi: flexSendNativeFacet.abi, name: abi.name });
-        const signature = toFunctionSignature(item);
-        const selector = toFunctionSelector(item);
-        console.log(`- ${selector}: ${signature}`);
-      }
-    });
-  
-    it('Should show FlexSendNativeDomainFacet function selectors', async function () {
-      const { flexSendNativeDomainFacet } = await loadFixture(deployFixture);
-
-      console.log('FlexSendNativeDomainFacet selectors:');
-      for (const abi of flexSendNativeDomainFacet.abi) {
-        if (abi.type !== 'function') {
-          continue;
-        }
-  
-        const item = getAbiItem({ abi: flexSendNativeDomainFacet.abi, name: abi.name });
-        const signature = toFunctionSignature(item);
-        const selector = toFunctionSelector(item);
-        console.log(`- ${selector}: ${signature}`);
-      }
-    });
-
-    it('Should show FlexSendTimeFacet function selectors', async function () {
-      const { flexSendTimeFacet } = await loadFixture(deployFixture);
-
-      console.log('FlexSendTimeFacet selectors:');
-      for (const abi of flexSendTimeFacet.abi) {
-        if (abi.type !== 'function') {
-          continue;
-        }
-
-        const item = getAbiItem({ abi: flexSendTimeFacet.abi, name: abi.name });
-        const signature = toFunctionSignature(item);
-        const selector = toFunctionSelector(item);
-        console.log(`- ${selector}: ${signature}`);
-      }
-    });
-
-    it('Should show FlexSendHashFacet function selectors', async function () {
-      const { flexSendHashFacet } = await loadFixture(deployFixture);
-
-      console.log('FlexSendHashFacet selectors:');
-      for (const abi of flexSendHashFacet.abi) {
-        if (abi.type !== 'function') {
-          continue;
-        }
-
-        const item = getAbiItem({ abi: flexSendHashFacet.abi, name: abi.name });
-        const signature = toFunctionSignature(item);
-        const selector = toFunctionSelector(item);
-        console.log(`- ${selector}: ${signature}`);
-      }
-    });
-
-  //
-  } else {
-  //
-
-    it('Should show FlexSendStandalone code', async function () {
-      const { publicClient, flex } = await loadFixture(deployFixture);
-
-      const code = await publicClient.getCode({ address: flex.address });
-      console.log(`FlexSendStandalone code: ${code}`);
-    });
-
-    it('Should show FlexSendStandalone function selectors', async function () {
-      const { flexSendHashFacet } = await loadFixture(deployFixture);
-
-      console.log('FlexSendStandalone selectors:');
-      for (const abi of flexSendHashFacet.abi) {
-        if (abi.type !== 'function') {
-          continue;
-        }
-
-        const item = getAbiItem({ abi: flexSendHashFacet.abi, name: abi.name });
-        const signature = toFunctionSignature(item);
-        const selector = toFunctionSelector(item);
-        console.log(`- ${selector}: ${signature}`);
-      }
-    });
-
-  //
-  }
-  //
-
   it('Should send native', async function () {
-    const {
-      flex,
-      flexSendNativeFacet,
-      flexSendNativeDomainFacet,
-      flexSendTimeFacet,
-      flexSendHashFacet,
-      walletClient,
-      resolverClient,
-      publicClient,
-    } = await loadFixture(deployFixture);
+    const { publicClient, walletClient, resolverClient, flex } = await loadFixture(deployFixture);
 
     const start = 123_456;
     const duration = 4_000_000_000n;
-    const nonce = 424_242n;
     const group = 0;
     const sender = resolverClient.account.address;
     const receiver = walletClient.account.address;
     const amount = 123_456_789n;
 
-    const sendDomain = await publicClient.readContract({
-      abi: flexSendNativeDomainFacet.abi,
+    const sendNativeDomain = await publicClient.readContract({
+      abi: flex.abi,
       address: flex.address,
       functionName: 'flexSendNativeDomain',
       args: [],
     });
-    const sendData0 = flexEncodeSendNativeData0({
+    const sendNativeData = flexEncodeSendNativeData({
+      sender,
+      receiver,
+      amount,
       start,
       duration,
-      sender,
-    });
-    const sendData1 = flexEncodeSendNativeData1({
       group,
-      nonce,
-      receiver,
     });
-    const sendData2 = flexEncodeSendNativeData2({
-      amount,
-    });
-    const sendHash = flexCalcSendNativeHash({
-      domain: sendDomain,
-      data0: sendData0,
-      data1: sendData1,
-      data2: sendData2,
+    const sendNativeHash = flexCalcSendNativeHash({
+      domain: sendNativeDomain,
+      data: sendNativeData,
     });
 
     const imaginaryComponentHashes: Hex[] = [];
@@ -315,19 +65,15 @@ describe('FlexSendNativeFacet', function () {
       imaginaryComponentHashes.push(imaginaryComponentHash);
     }
 
-    const componentHashes = [sendHash, ...imaginaryComponentHashes];
+    const componentHashes = [sendNativeHash, ...imaginaryComponentHashes];
     const orderTree = flexCalcTree({ leaves: componentHashes });
     const orderHash = flexCalcTreeHash({ tree: orderTree });
-
-    const sendComponentBranch = flexCalcSendNativeBranch({
-      tree: orderTree,
-      sendNativeHash: sendHash,
-    });
+    const sendNativeBranch = flexCalcBranch({ tree: orderTree, leaf: sendNativeHash });
 
     let expectedSendHash: Hex;
     {
       const time = await publicClient.readContract({
-        abi: flexSendTimeFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendTime',
         args: [
@@ -340,7 +86,7 @@ describe('FlexSendNativeFacet', function () {
       expectedSendHash = zeroAddress;
 
       const hash = await publicClient.readContract({
-        abi: flexSendHashFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendHash',
         args: [
@@ -355,13 +101,12 @@ describe('FlexSendNativeFacet', function () {
       const balanceBefore = await publicClient.getBalance({ address: receiver });
 
       const hash = await resolverClient.writeContract({
-        abi: flexSendNativeFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendNative',
         args: [
-          sendData0,
-          sendData1,
-          sendComponentBranch,
+          sendNativeData.sendData[1],
+          sendNativeBranch,
         ],
         value: amount,
       });
@@ -369,13 +114,23 @@ describe('FlexSendNativeFacet', function () {
       const receipt = await publicClient.getTransactionReceipt({ hash });
       console.log(`flexSendNative gas (1st): ${receipt.gasUsed}`);
 
+      const logs = parseEventLogs({
+        abi: flex.abi,
+        logs: receipt.logs,
+        eventName: 'FlexSend',
+        args: {
+          orderHash,
+        },
+      });
+      expect(logs.length).equal(1);
+
       const balance = await publicClient.getBalance({ address: receiver });
       expect(balance - balanceBefore).equal(amount);
     }
 
     {
       const time = await publicClient.readContract({
-        abi: flexSendTimeFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendTime',
         args: [
@@ -388,7 +143,7 @@ describe('FlexSendNativeFacet', function () {
       expectedSendHash = flexCalcAccumulatorHash({ hashBefore: expectedSendHash, hashToAdd: orderHash });
 
       const hash = await publicClient.readContract({
-        abi: flexSendHashFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendHash',
         args: [
@@ -400,35 +155,31 @@ describe('FlexSendNativeFacet', function () {
     }
 
     {
-      const sendData0 = flexEncodeSendNativeData0({
+      const newSendNativeData = flexEncodeSendNativeData({
+        sender,
+        receiver,
+        amount,
         start: start - 1, // Note - bad start
         duration,
-        sender,
+        group,
       });
-      const sendHash = flexCalcSendNativeHash({
-        domain: sendDomain,
-        data0: sendData0,
-        data1: sendData1,
-        data2: sendData2,
+      const newSendNativeHash = flexCalcSendNativeHash({
+        domain: sendNativeDomain,
+        data: newSendNativeData,
       });
   
-      const componentHashes = [sendHash, ...imaginaryComponentHashes];
-      const orderTree = flexCalcTree({ leaves: componentHashes });
-
-      const sendComponentBranch = flexCalcSendNativeBranch({
-        tree: orderTree,
-        sendNativeHash: sendHash,
-      });
+      const newComponentHashes = [newSendNativeHash, ...imaginaryComponentHashes];
+      const newOrderTree = flexCalcTree({ leaves: newComponentHashes });
+      const newSendNativeBranch = flexCalcBranch({ tree: newOrderTree, leaf: newSendNativeHash });
 
       await expect(
         resolverClient.writeContract({
-          abi: flexSendNativeFacet.abi,
+          abi: flex.abi,
           address: flex.address,
           functionName: 'flexSendNative',
           args: [
-            sendData0,
-            sendData1,
-            sendComponentBranch,
+            newSendNativeData.sendData[1],
+            newSendNativeBranch,
           ],
           value: amount,
         }),
@@ -441,35 +192,31 @@ describe('FlexSendNativeFacet', function () {
     {
       const balanceBefore = await publicClient.getBalance({ address: receiver });
 
-      const sendData0 = flexEncodeSendNativeData0({
+      const newSendNativeData = flexEncodeSendNativeData({
+        sender,
+        receiver,
+        amount,
         start: start + 1, // Note - new start
         duration,
-        sender,
+        group,
       });
-      const sendHash = flexCalcSendNativeHash({
-        domain: sendDomain,
-        data0: sendData0,
-        data1: sendData1,
-        data2: sendData2,
+      const newSendNativeHash = flexCalcSendNativeHash({
+        domain: sendNativeDomain,
+        data: newSendNativeData,
       });
-  
-      const componentHashes = [sendHash, ...imaginaryComponentHashes];
-      const orderTree = flexCalcTree({ leaves: componentHashes });
-      newOrderHash = flexCalcTreeHash({ tree: orderTree });
 
-      const sendComponentBranch = flexCalcSendNativeBranch({
-        tree: orderTree,
-        sendNativeHash: sendHash,
-      });
+      const newComponentHashes = [newSendNativeHash, ...imaginaryComponentHashes];
+      const newOrderTree = flexCalcTree({ leaves: newComponentHashes });
+      newOrderHash = flexCalcTreeHash({ tree: newOrderTree });
+      const newSendNativeBranch = flexCalcBranch({ tree: newOrderTree, leaf: newSendNativeHash });
 
       const hash = await resolverClient.writeContract({
-        abi: flexSendNativeFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendNative',
         args: [
-          sendData0,
-          sendData1,
-          sendComponentBranch,
+          newSendNativeData.sendData[1],
+          newSendNativeBranch,
         ],
         value: amount,
       });
@@ -477,13 +224,23 @@ describe('FlexSendNativeFacet', function () {
       const receipt = await publicClient.getTransactionReceipt({ hash });
       console.log(`flexSendNative gas (2nd): ${receipt.gasUsed}`);
 
+      const logs = parseEventLogs({
+        abi: flex.abi,
+        logs: receipt.logs,
+        eventName: 'FlexSend',
+        args: {
+          orderHash: newOrderHash,
+        },
+      });
+      expect(logs.length).equal(1);
+
       const balance = await publicClient.getBalance({ address: receiver });
       expect(balance - balanceBefore).equal(amount);
     }
 
     {
       const time = await publicClient.readContract({
-        abi: flexSendTimeFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendTime',
         args: [
@@ -496,7 +253,7 @@ describe('FlexSendNativeFacet', function () {
       expectedSendHash = flexCalcAccumulatorHash({ hashBefore: expectedSendHash, hashToAdd: newOrderHash });
 
       const hash = await publicClient.readContract({
-        abi: flexSendHashFacet.abi,
+        abi: flex.abi,
         address: flex.address,
         functionName: 'flexSendHash',
         args: [
