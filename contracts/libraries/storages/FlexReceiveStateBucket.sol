f3809bc5fd49f5730f54c72405cfbb5ac408733b
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-18 21:09:45 +0400

    Add bucket content comment

--- contracts/libraries/storages/FlexReceiveStateBucket.sol
+++ contracts/libraries/storages/FlexReceiveStateBucket.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8.28;
 
 library FlexReceiveStateBucket {
+    // Content:
+    // - bucket: receiver (160), nonce (96)
+
     function calcBucket(address receiver_, uint96 nonce_) internal pure returns (bytes32) {
         return bytes20(receiver_) | bytes32(uint256(nonce_) / 48); // 96 bit / 2 bit
     }
--- contracts/libraries/storages/FlexSendStateBucket.sol
+++ contracts/libraries/storages/FlexSendStateBucket.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8.28;
 
 library FlexSendStateBucket {
+    // Content:
+    // - bucket: sender (160), group (96)
+
     function calcBucket(address sender_, uint96 group_) internal pure returns (bytes32) {
         return bytes20(sender_) | bytes32(uint256(group_));
     }
