345b686071d2a3920acfa37dd38b3893ecae64ba
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-26 20:38:04 +0400

    Save send bucket with current time override

--- contracts/facets/FlexSaveSendFacet.sol
+++ contracts/facets/FlexSaveSendFacet.sol
@@ -7,6 +7,7 @@ import {IFlexSaveSend} from "../interfaces/IFlexSaveSend.sol";
 import {FlexSendSave} from "../interfaces/events/FlexSendSave.sol";
 
 import {FlexSaveSendData} from "../libraries/data/FlexSaveSendData.sol";
+import {FlexSendBucketStateData} from "../libraries/data/FlexSendBucketStateData.sol";
 
 import {FlexSendStateBucket} from "../libraries/storages/FlexSendStateBucket.sol";
 import {FlexSendStateStorage} from "../libraries/storages/FlexSendStateStorage.sol";
@@ -19,6 +20,7 @@ contract FlexSaveSendFacet is IFlexSaveSend {
     function flexSaveSend(bytes32 saveData0_) external override {
         bytes32 bucket = FlexSendStateBucket.calcBucket(FlexSaveSendData.readSender(saveData0_), FlexSaveSendData.readGroup(saveData0_));
         bytes32 bucketState = FlexSendStateStorage.data()[bucket];
+        bucketState = FlexSendBucketStateData.writeTime(bucketState, uint48(block.timestamp));
         bytes32 saveBucket = FlexSendSaveStateBucket.calcBucket(msg.sender, FlexSaveSendData.readSlot(saveData0_));
         FlexSendSaveStateStorage.data()[saveBucket] = FlexEfficientHash.calc(bucket, bucketState);
         emit FlexSendSave(bucket, bucketState, saveBucket);
