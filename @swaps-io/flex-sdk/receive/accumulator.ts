405272397950a358ae1a401cf5456c726d07963d
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-28 18:51:59 +0400

    Extract encode accumulator hash for send & receive in lib

--- @swaps-io/flex-sdk/receive/accumulator.ts
+++ @swaps-io/flex-sdk/receive/accumulator.ts
@@ -1,12 +1,21 @@
-import { Hex } from '../external';
+import { asHex, AsHexValue, Hex } from '../external';
 
 import { flexCalcAccumulatorHash } from '../accumulator';
 
+export interface FlexEncodeReceiveAccumulatorHashParams {
+  orderHash: AsHexValue;
+}
+
+export function flexEncodeReceiveAccumulatorHash({ orderHash }: FlexEncodeReceiveAccumulatorHashParams): Hex {
+  return asHex(orderHash, 32);
+}
+
 export interface FlexCalcReceiveAccumulatorHashParams {
-  hashBefore: Hex;
-  orderHash: Hex;
+  hashBefore: AsHexValue;
+  orderHash: AsHexValue;
 }
 
 export function flexCalcReceiveAccumulatorHash({ hashBefore, orderHash }: FlexCalcReceiveAccumulatorHashParams): Hex {
-  return flexCalcAccumulatorHash({ hashBefore, hashToAdd: orderHash });
+  const hashToAdd = flexEncodeReceiveAccumulatorHash({ orderHash });
+  return flexCalcAccumulatorHash({ hashBefore, hashToAdd });
 }
--- @swaps-io/flex-sdk/send/accumulator.ts
+++ @swaps-io/flex-sdk/send/accumulator.ts
@@ -2,16 +2,25 @@ import { asHex, AsHexValue, concatHex, Hex, sliceHex } from '../external';
 
 import { flexCalcAccumulatorHash } from '../accumulator';
 
+export interface FlexEncodeSendAccumulatorHashParams {
+  orderHash: AsHexValue;
+  start: AsHexValue;
+}
+
+export function flexEncodeSendAccumulatorHash({ orderHash, start }: FlexEncodeSendAccumulatorHashParams): Hex {
+  return concatHex([
+    sliceHex(asHex(orderHash, 32), 0, 26),
+    asHex(start, 6),
+  ]);
+}
+
 export interface FlexCalcSendAccumulatorHashParams {
-  hashBefore: Hex;
-  orderHash: Hex;
+  hashBefore: AsHexValue;
+  orderHash: AsHexValue;
   start: AsHexValue;
 }
 
 export function flexCalcSendAccumulatorHash({ hashBefore, orderHash, start }: FlexCalcSendAccumulatorHashParams): Hex {
-  const hashToAdd = concatHex([
-    sliceHex(orderHash, 0, 26),
-    asHex(start, 6),
-  ]);
+  const hashToAdd = flexEncodeSendAccumulatorHash({ orderHash, start });
   return flexCalcAccumulatorHash({ hashBefore, hashToAdd });
 }
