37cd75b867275b3d3e4ab373a8e2674e071a1d88
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-02-28 17:54:44 +0400

    Add missing send save proofer test

--- test/FlexSendProofVerifier.ts
+++ test/FlexSendProofVerifier.ts
@@ -8,6 +8,7 @@ import {
   flexAssignComponentDomain,
   flexCalcAccumulatorBranch,
   flexCalcBranch,
+  flexCalcBranchHash,
   flexCalcSendNativeHash,
   flexCalcSendTokenHash,
   flexCalcTree,
@@ -442,7 +443,6 @@ describe('FlexSendProofVerifier', function () {
       saveBucket,
       saveTime,
     });
-    console.log({ proof });
 
     const sig = FLEX_SEND_EVENT_SIGNATURE;
     const hash = zeroHash;
@@ -452,6 +452,79 @@ describe('FlexSendProofVerifier', function () {
       verifier.read.verifyHashEventProof([sig, hash, chain, proof]),
     ).rejectedWith(
       'FlexProofHashError()',
+    );
+  });
+
+  it('Should not verify proof with missing send save', async function () {
+    const { verifier, testChainId } = await loadFixture(deployFixture);
+
+    const start = 123_456;
+    const duration = 4_003_002_001n;
+    const group = 777;
+    const sender = '0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef';
+    const receiver = '0xc0debeefc0debeefc0debeefc0debeefc0debeef';
+    const token = '0xf00baff00baff00baff00baff00baff00baff00b'
+    const amount = 123_456_789n;
+
+    const sendDomain = '0xd0111a11d0111a11';
+    const sendData = flexEncodeSendTokenData({
+      sender,
+      receiver,
+      amount,
+      start,
+      duration,
+      group,
+      token,
+    });
+    const sendHash = flexCalcSendTokenHash({ domain: sendDomain, data: sendData });
+
+    const orderHashes: Hex[] = [
+      sendHash,
+      '0xe93c632bf8243f5ce2d70d0d13e76a868573c74437cdc32fea4c0e3afccd6523',
+      '0x9e3cefcd2210a275f810c0e956471e061830088607b4db84becbba153a283392',
+      '0x5fafb9728bd8166de4388cc96e76776c11b84133f8a2db79df9f5dc8d1a15ec0',
+      '0x81a7287cb6819d35be18e8141820f920e005da0c0832895bbdd7c90595259794',
+      '0x70ab6d7fb5e05d8f3cfe66c2d92ad16063dbc36e90aad06df4ad2021924f6ef6',
+    ];
+    const orderTree = flexCalcTree({ leaves: orderHashes });
+    const orderBranch = flexCalcBranch({ tree: orderTree, leaf: sendHash });
+    const orderHash = flexCalcBranchHash({ branch: orderBranch, leaf: sendHash });
+
+    const variant = 133713371337n;
+    const saver = '0x5a7ef55a7ef55a7ef55a7ef55a7ef55a7ef55a7e';
+    const slot = 999;
+    const saveBucket = flexEncodeSendSaveBucket({ saver, slot });
+    const saveTime = 1_750_750_750;
+
+    const branch = flexCalcAccumulatorBranch({
+      branch: orderBranch,
+      hashBefore: '0xbef0bef0bef0bef0bef0bef0bef0bef0bef0bef0',
+      hashesAfter: [
+        '0xafafafaf11111111111111111111111111111111111111111111111111111111',
+        '0xafafafaf22222222222222222222222222222222222222222222222222222222',
+        '0xafafafaf33333333333333333333333333333333333333333333333333333333',
+        '0xafafafaf44444444444444444444444444444444444444444444444444444444',
+        '0xafafafaf55555555555555555555555555555555555555555555555555555555',
+      ],
+    });
+
+    const proof = flexEncodeSendProof({
+      variant,
+      domain: sendDomain,
+      data: sendData,
+      branch,
+      saveBucket,
+      saveTime,
+    });
+
+    const sig = FLEX_SEND_EVENT_SIGNATURE;
+    const hash = orderHash;
+    const chain = testChainId;
+
+    await expect(
+      verifier.read.verifyHashEventProof([sig, hash, chain, proof]),
+    ).rejectedWith(
+      'FlexProofSendSaveError()',
     );
   });
 });
