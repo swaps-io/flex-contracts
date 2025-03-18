302097271c7db124ab88a284ed2c3043fb37919f
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-03-18 17:46:25 +0400

    Bump deps

--- test/before.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-
-import { flexInit, flexReady } from '@swaps-io/flex-sdk';
-
-before(async function() {
-  await flexInit; // CJS target init hassle
-  expect(flexReady()).equal(true); // Ensure deps ok
-});
